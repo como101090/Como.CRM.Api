9bb597cb990d7e28f4e3599ab78c2c1207485ed7
Admin <Admin@DESKTOP-AVOI80J>
2026-06-30 12:27:53 +0400

    add migracion miniChange_Vs2

---
+++ Migrations/20260630082607_miniChange_VS2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Como.CRM.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class miniChange_VS2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PublisherMailInfo",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PublisherMailInfo",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "xojljswkfupixqig");
+        }
+    }
+}
